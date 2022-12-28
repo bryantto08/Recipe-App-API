94e94ff83aad4a9a7ea0d3e419f65effa9a9da39
bryantto08 <bryantto08@gmail.com>
2022-12-28 14:16:53 -0500

    implement user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,30 @@
-from django.db import models  # noqa
+# Database models
 
-# Create your models here.
+from django.db import models 
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    # Manager for Users
+
+    def create_user(self, email, password=None, **extra_fields):
+        # Create, Save and return a new user
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)  # best practice to save user in case there are multiple dbs
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # User in the system
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()  # How to connect a Model Manager to a model
+
+    USERNAME_FIELD = 'email'
