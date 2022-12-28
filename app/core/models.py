52949c1442ed6d19cf26952076f9fac008763e94
bryantto08 <bryantto08@gmail.com>
2022-12-28 14:46:30 -0500

    superuser support, normalize email addresses in user model

--- app/core/models.py
+++ app/core/models.py
@@ -13,10 +13,22 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         # Create, Save and return a new user
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User must have an Email address!")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         #  ^ best practice to save user in case there are multiple dbs
+
+        return user
+
+
+    def create_superuser(self, email, password):
+        # Create and return a new superuser
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
