4a485dd177805421bcec4ea63e41c4485a46d7d6
bryantto08 <bryantto08@gmail.com>
2022-12-29 12:58:42 -0500

    Setup Django Admin:

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
 
 from core import models
 
@@ -9,6 +10,38 @@ class UserAdmin(BaseUserAdmin):
     # Define the admin pages for users
     ordering = ["id"]
     list_display = ["email", "name"]
+
+    # Supporting CRUD
+    fieldsets = (
+        (None,  {"fields": ("email", "password")}),
+        (
+            translate("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            }
+        ),
+        (translate("Important Dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]  # makes last_login field readonly
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),  # Class (wide) adds slight change with css
+            "fields": (
+                "email",
+                "password1",
+                "password2",
+                "name",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+            )
+        }),
+    )
 
 """
 Adds User Model to the Admin Page, we format it using our custom 
