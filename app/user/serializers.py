c077e692af04169f113f5e37f964ad30f1b400ef
bryantto08 <bryantto08@gmail.com>
2022-12-30 17:39:34 -0500

    Built User API

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -17,9 +17,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["email", "password", "name"]  # Fields that user can change
         extra_kwargs = {"password" : { "write_only": True, "min_length": 5}}
 
-    def create (self, validated_data):
+    def create(self, validated_data):
         """ Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
@@ -45,3 +54,4 @@ class AuthTokenSerializer(serializers.Serializer):
             # View will translate this into HTTP 400 response ^
         attrs["user"] = user
         return attrs
+
--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 """
 Views for the user API
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import (
@@ -19,3 +19,16 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new aut token for user"""
     serializer_class = AuthTokenSerializer
     rendered_classes = api_settings.DEFAULT_RENDERER_CLASSES  # Browsable API
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the Authenticated User"""
+    serializer_class = UserSerializer
+    # Authentication: Is the user who they say they are
+    # Permissions: What is the user allowed to do
+    # We auth using TokenAuth, Only requirement for permission is Auth
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
